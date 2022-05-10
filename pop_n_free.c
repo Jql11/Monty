5fa8925ede9a0fde243ecbf5f23eaf3346f8f50e
jql11 <4325@holbertonschool.com>
2022-05-10 07:00:50 -0700

    add nop

--- pop_n_free.c
+++ pop_n_free.c
@@ -40,4 +40,13 @@ void pop(stack_t **stack, unsigned int line_number)
 		free(current);
 	}
 }
-
+/**
+ * nop - do nothing
+ * @h: head of list
+ * @line_number: bytecode line number
+ */
+void nop(stack_t **h, unsigned int line_number)
+{
+	(void) h;
+	(void) line_number;
+}
