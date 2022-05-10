48666714072a2167c517256c83ae4971f0b917a8
jql11 <4325@holbertonschool.com>
2022-05-09 22:31:52 -0700

    add file

---
+++ pop_n_free.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * free_dlistint - frees a dlistint_t
+ *@head: head
+ * Return: void
+ */
+void free_dlistint(stack_t **head)
+{
+	stack_t *tmp;
+
+	tmp = *head
+	while (*head != NULL)
+	{
+		head = (*head)->next;
+		free((tmp);
+	}
+}
+/**
+ * pop - deletes top node of doubly linked list
+ * @h: pointer to head of doubly linked list
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current;
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(STDERR_FILENO, "L<%u>: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	current = *stack;
+	if (current->next == NULL)
+	{
+		*stack = (*stack)->next;
+		free(current);
+	}
+	else
+	{
+		current->next->prev = NULL;
+		free(current);
+	}
+}
+
