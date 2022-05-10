dc4f3ad2a9e2f99a681d9a3bcfb5cd9168b204f9
Farah <farah.mccurdy@gmail.com>
2022-05-09 22:37:47 -0700

    add: pint func

--- push_pall.c
+++ push_pall.c
@@ -24,3 +24,26 @@ void pall(stack_t **stack, unsigned int line_number)
 		temp = temp->next;
 	}
 }
+
+/**
+ * pint - print value of top stack
+ * @stack: the stack to assess
+ * @line_number: line number
+ *
+ * Description: print value of top stack
+ *
+ * Return: void
+ */
+
+void pint(stack_t **stack, unsigned int line_number);
+{
+	stack *temp = *stack;
+
+	if (stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", temp->n);
+}
