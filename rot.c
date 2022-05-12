f174cd4944ba6145ae087a2a67a85083fcc15440
Farah <farah.mccurdy@gmail.com>
2022-05-11 22:28:32 -0700

    add: rotr func

--- rot.c
+++ rot.c
@@ -15,13 +15,41 @@ void rotl(stack_t **stack, unsigned int line_number)
 	int value = (*stack)->n;
 	(void)line_number;
 
-	if (*stack == NULL || stack == NULL)
+	if (*stack == NULL || stack == NULL || *stack->next == NULL)
 		return;
 
 	while (temp->next != NULL)
 	{
 		temp->n = temp->next->n;
 		temp = temp->next;
+	}
+	temp->n = value;
+}
+
+/**
+ * rotr - rotate stack to bottom
+ * @stack: the stack to rotate
+ * @line_number: line number in monty, not used
+ *
+ * Description: last element becomes first
+ */
+
+void rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+	int value = (*stack)->n;
+	(void)line_number;
+
+	if (*stack == NULL || stack == NULL || *stack->next == NULL)
+		return;
+
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	while (temp->prev != NULL)
+	{
+		temp->n = temp->prev->n;
+		temp = temp->prev;
 	}
 	temp->n = value;
 }
